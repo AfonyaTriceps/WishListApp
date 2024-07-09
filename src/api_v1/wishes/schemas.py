d756dfdec006f17474ea6a8a0cf7e21b1923b7e7
AfonyaTriceps <d.vlad957@gmail.com>
2024-07-09 22:34:10 +0300

    Добавил схемы.

---
+++ src/api_v1/wishes/schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class BaseWish(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    name: str
+    url: str
+    description: str
+    price: float
+    photo: bytes
+
+
+class Wish(BaseWish):
+    id: int
+
+
+class CreateWish(Wish):
+    pass
+
