efa46ccce4911b60e3d98840bd5b22e47407da7a
AfonyaTriceps <d.vlad957@gmail.com>
2024-07-09 22:35:00 +0300

    Класс настроек приложения.

---
+++ src/api_v1/settings.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+env_file = f'{Path(__file__).parent.parent.parent}/.env'
+
+
+class Settings(BaseSettings):
+    """Класс настроек приложения"""
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_PORT: str
+    DB_HOST: str
+
+    api_v1_prefix: str = '/api/v1'
+
+    model_config = SettingsConfigDict(env_file=env_file, env_file_encoding='utf-8', extra='ignore')
+
+    @property
+    def db_url(self) -> str:
+        """Возвращает URL для подключения к базе данных"""
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+
+settings = Settings()
