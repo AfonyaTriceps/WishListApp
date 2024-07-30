042b8821c7d45244e1bfa151f000ad3274dea4e5
AfonyaTriceps <d.vlad957@gmail.com>
2024-07-30 03:17:24 +0300

    Подключил auth_router из fastapi_users.

--- src/api_v1/users/auth/__init__.py
+++ src/api_v1/users/auth/__init__.py
@@ -1,3 +1,9 @@
 from fastapi import APIRouter
 
+from src.api_v1.users.auth.backend import auth_backend
+from src.api_v1.users.auth.routers import fastapi_users
+
 auth_router = APIRouter(tags=['Auth'])
+auth_router.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+)
